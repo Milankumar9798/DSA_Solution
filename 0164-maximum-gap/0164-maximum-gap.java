ca626bd73f9029401a42ff8e52365440ae2234a0
Milankumar9798 <milankumar979872@gmail.com>
2026-07-14 14:34:23 +0530

    Time: 45 ms (62.54%), Space: 91.2 MB (15.96%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -1,20 +1,10 @@
 class Solution {
     public int maximumGap(int[] nums) {
         Arrays.sort(nums);
-        if(nums.length==1 && nums.length==0){
-            return 0;
-        }
-        int sum=0;
+        if(nums.length<2) return 0;
         int ans=0;
-        for(int i=0;i<nums.length;i++){
-            int j=i+1;
-            if(j<nums.length){
-                if(nums[j]>nums[i]){
-                    sum=nums[j]-nums[i];
-                    ans=Math.max(sum,ans);
-                    sum=0;
-                }
-            }
+        for(int i=1;i<nums.length;i++){
+            ans=Math.max(ans,nums[i]-nums[i-1]);
         }
         return ans;
     }
