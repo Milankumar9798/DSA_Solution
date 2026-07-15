d90e6c93892dacfb09f878c6b0761d4af438e574
Milankumar9798 <milankumar979872@gmail.com>
2026-07-15 21:58:05 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.2%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -3,9 +3,8 @@ class Solution {
         if(n==0){
             return 0;
         }
-        int mid= (int)Math.pow(2,n-1);
-        // int mid =len/2;
-        // int mid = 1 << (n - 1);
+        // int mid= (int)Math.pow(2,n-1);
+        int mid = 1 << (n - 1);
         if(k<=mid){
             return kthGrammar(n-1,k);
         }else{
