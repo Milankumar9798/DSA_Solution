dddf6b8ca51dd4af4b462288a51646ba7679b019
Milankumar9798 <milankumar979872@gmail.com>
2026-08-02 13:28:27 +0530

    Time: 0 ms (100%), Space: 43.1 MB (62.03%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int winner(int l,int r,int[] nums,Integer[][] dp){
-        if(l==r){
-            return nums[l];
-        }
-        if(dp[l][r]!=null){
-            return dp[l][r];
-        }
-        int left=nums[l]-winner(l+1,r,nums,dp);
-        int right=nums[r]-winner(l,r-1,nums,dp);
-        return dp[l][r]=Math.max(left,right);
-    }
+    // public int winner(int l,int r,int[] nums,Integer[][] dp){
+    //     if(l==r){
+    //         return nums[l];
+    //     }
+    //     if(dp[l][r]!=null){
+    //         return dp[l][r];
+    //     }
+    //     int left=nums[l]-winner(l+1,r,nums,dp);
+    //     int right=nums[r]-winner(l,r-1,nums,dp);
+    //     return dp[l][r]=Math.max(left,right);
+    // }
     public boolean stoneGame(int[] piles) {
-        int n=piles.length;
-        int l=0;
-        int r=n-1;
-        Integer[][] dp=new Integer[n][n];
-        return winner(l,r,piles,dp)>=0;
+        return true;
+        // int n=piles.length;
+        // int l=0;
+        // int r=n-1;
+        // Integer[][] dp=new Integer[n][n];
+        // return winner(l,r,piles,dp)>=0;
     }
 }
